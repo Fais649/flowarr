0bdccb21e295f6944f633e4248c147839649e390
Faisal Alalaiwat <fais649@proton.me>
2026-07-31 15:30:08 +0200

    fix wayfinder generate for publish action

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
             formVariants: true,
             command: process.env.SKIP_WAYFINDER === 'true'
                 ? 'true'
-                : 'php artisan wayfinder:generate',
+                : 'php artisan wayfinder:generate --with-form'
         }),
     ],
 });
